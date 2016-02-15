0fed96c571d44fa15c8942ae55fea08ec07d11d8
John Kelly <kellyj58@tcd.ie>
2016-02-15 15:59:16 +0000

    Added basic functionality for post creator and authentication

--- app.js
+++ app.js
@@ -2,13 +2,27 @@ var express = require("express");
 var bp = require("body-parser");
 var jade = require("jade");
 var fs = require("fs");
+var cookieParser = require("cookie-parser");
 var app = express();
+app.use(cookieParser())
+var config;
+try {
+  config = JSON.parse(fs.readFileSync("config.json", "utf8"));
+} catch(e) {
+  config = "{\n\t\"username\": \"user\",\n\t\"password\": \"pass\"\n}";
+  fs.writeFileSync("config.json", config, "utf8");
+}
+
+var auth = require("node-auth")(config);
 var posts;
+var nextPost;
 try {
   posts = JSON.parse(fs.readFileSync("posts.json", "utf8"));
+  nextPost = posts.nextPost || 0;
+  posts = posts.posts;
 } catch(e) {
   console.log(e);
-  fs.writeFileSync("posts.json", "[]", "utf8");
+  fs.writeFileSync("posts.json", "{}", "utf8");
   process.exit();
 }
 app.set("views", __dirname + "/views");
@@ -37,7 +51,7 @@ app.post("/post/:id/addComment", function(req, res) {
     posted: getPostTime()
   }
   var commentId = addComment(id, comment);
-  if(commentId) {
+  if(commentId !== false) {
     res.redirect("/post/" + id +"#comment-" + commentId);
   } else {
     res.send(`
@@ -53,6 +67,9 @@ app.post("/post/:id/addComment", function(req, res) {
 });
 
 function searchTitles(search) {
+  console.log("key: %s", search);
+  if(search == "")
+    return [];
   var results = [];
   for(var i = 0; i < posts.length; i++ ) {
     if(posts[i].title.indexOf(search) != -1) {
@@ -62,14 +79,77 @@ function searchTitles(search) {
   return results;
 }
 
+function searchAll(search) {
+  var results = [];
+  if(search == "")
+    return [];
+  for(var i = 0; i < posts.length; i++ ) {
+    if(posts[i].title.indexOf(search) != -1) {
+      results.push(posts[i]);
+    }
+    for(var j = 0 ; j < posts[i].body.length; j++) {
+      if(posts[i].body[j].indexOf(search)) {
+        results.push(posts[i]);
+      }
+    }
+  }
+  return results;
+}
+
+function pruneDuplicates(arr) {
+  var results = [];
+  var ids = [];
+  for(var i = 0 ; i < arr.length; i++) {
+    if(ids.indexOf(arr[i].id) == - 1) {
+      results.push(arr[i]);
+      ids.push(arr[i].id)
+    }
+  }
+  return results;
+}
+
+function searchAllKeys(str, titlesOnly) {
+  var keys = str.split(" ");
+  var results = [];
+  if(titlesOnly) {
+    for(var i = 0; i < keys.length; i++ ) {
+      var tmp = searchTitles(keys[i]);
+      for(var j = 0 ; j < tmp.length; j++) {
+        results.push(tmp[j]);
+      }
+    }
+  } else {
+    for(var i = 0 ; i < keys.length; i++) {
+      var tmp = searchAll(keys[i]);
+      for(var j = 0 ; j < tmp.length; j++) {
+        results.push(tmp[j]);
+      }
+    }
+  }
+  return pruneDuplicates(results);
+}
+
 app.post("/search", function(req, res) {
   if(req.body.search == "Titles") {
-    var search = searchTitles(req.body.query);
-    res.render('index', { posts: search, singleView: false });
-  } else {
-    res.send("not implemented yet<br/>Sorry :P");
+    var search = searchAllKeys(req.body.query, true);
+      res.render('index', { posts: search, singleView: search.length == 1 });
+    } else {
+    var search = searchAllKeys(req.body.query, false);
+    res.render("index", { posts: search, singleView: search.length == 1 });
   }
 });
+
+app.get("/post", auth, function(req, res) {
+  res.render("post");
+});
+
+app.post("/post", auth, function(req, res) {
+
+});
+
+function addPost(post) {
+
+}
 
 function findPost(id) {
   for(var i = 0 ; i < posts.length; i++) {
